5c6c545f946dccd6e2a33ee40f3217b5f14cf5a2
Elisaveta Gribacheva <elizavetagribacheva57@gmail.com>
2025-10-16 19:01:05 +0300

    level1

--- Olympic Programming Club/task1.cpp
+++ Olympic Programming Club/task1.cpp
@@ -1,3 +1,42 @@
 //
 // Created by Elizaveta Gribacheva on 13.10.2025.
 //
+#include <iostream>
+#include  <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> v;
+
+    for (int i = 1; i <= n/2; i++) {
+        if (n % i == 0) {
+            int sum = 0;
+            int num = i;
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            v.emplace_back(sum,i);
+        }
+    }
+    int sum = 0;
+    int num = n;
+
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    v.emplace_back(sum, n);
+    sort(v.begin(), v.end(), [](
+        const pair<int, int>& a, const pair<int, int>& b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first > b.first;
+});
+
+    cout << v[0].second;
+    return 0;
+}
--- Olympic Programming Club/task2.cpp
+++ Olympic Programming Club/task2.cpp
@@ -1,3 +1,45 @@
 //
 // Created by Elizaveta Gribacheva on 13.10.2025.
 //
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio();
+    cin.tie(0);
+    int a,b,c;
+    cin >> a;
+    cin>> b;
+    cin >> c;
+
+    unordered_set<int> mp;
+    unordered_set<int> ab;
+
+    while (a != 0) {
+        int last = a%10;
+        a = a/10;
+        mp.insert(last);
+    }
+
+    while (b != 0) {
+        int last = b%10;
+        b = b/10;
+        auto got = mp.find(last);
+        if (got != mp.end()) ab.insert(last);
+    }
+set<int> an;
+    while (c != 0) {
+        int last = c%10;
+        c = c/10;
+        auto got = ab.find(last);
+        if (got != ab.end()) an.insert(last);
+    }
+    for (auto& i : an) {
+        cout << i;
+    }
+return 0;
+}
